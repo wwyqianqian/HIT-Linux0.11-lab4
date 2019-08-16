caa7a5e2d5001bef2da6468a03fe71eadf698e5e
wwyqianqian <1208331933@qq.com>
2019-08-16 20:51:32 +0800

    [fix]: sched.h fork.c sched.c system_call.s

--- linux-0.11/kernel/sched.c
+++ linux-0.11/kernel/sched.c
@@ -61,9 +61,10 @@ static union task_union init_task = {INIT_TASK,};
 long volatile jiffies=0;
 long startup_time=0;
 struct task_struct *current = &(init_task.task);
+struct tss_struct *tss = &(init_task.task.tss);
 struct task_struct *last_task_used_math = NULL;
 
-struct tss_struct *tss = &(init_task.task.tss);
+
 
 struct task_struct * task[NR_TASKS] = {&(init_task.task), };
 
